643b64ad646905a32f949512d7901051946cef94
eulerbakke <euler.bakke@gmail.com>
2021-10-14 16:37:00 -0300

    add start script

--- app.js
+++ app.js
@@ -122,7 +122,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-app.listen(3000, () => {
-    console.log("Serving on PORT 3000.")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on PORT ${port}`);
 })
 
